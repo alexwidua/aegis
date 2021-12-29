6f94048664e123f3c70a5d0d6234089fd05497b8
Alexander Widua <a.widua@gmail.com>
2021-12-29 17:14:30 +0100

    content: update copy

--- src/views/InitialView.js
+++ src/views/InitialView.js
@@ -4,6 +4,7 @@ const InitialView = () => {
 	return (
 		<div className={styles.container}>
 			{/* <div className={styles.logo} /> */}
+			<h1>Aegis.</h1>
 			<h2>A tiny game to practice AoE building shortcuts.</h2>
 		</div>
 	)
