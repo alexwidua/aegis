888ae6e84a1b3c41414cf3e709daeb1c4d5add54
Alexander Widua <a.widua@gmail.com>
2021-12-29 12:05:27 +0100

    content: update landing page copy

--- src/views/InitialView.js
+++ src/views/InitialView.js
@@ -3,10 +3,8 @@ import styles from './initial.module.scss'
 const InitialView = () => {
 	return (
 		<div className={styles.container}>
-			<div className={styles.logo} />
-			<h2>
-				XYZ is a small game to learn or practice AoE building shortcuts.
-			</h2>
+			{/* <div className={styles.logo} /> */}
+			<h2>A tiny game to practice AoE building shortcuts.</h2>
 		</div>
 	)
 }
