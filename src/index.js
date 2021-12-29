cfa366b8b4b5098f0ea3857b7020952cd0b5af4a
Alexander Widua <a.widua@gmail.com>
2021-12-29 13:13:18 +0100

    fix: misc

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import './styles/base.scss'
 import './styles/animation.css'
 import App from './App'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
