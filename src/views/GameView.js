2b2e622563321e59eb05e09406b31e79a2c6e075
Alexander Widua <a.widua@gmail.com>
2021-12-29 13:11:20 +0100

    feat: add transitions

--- src/views/GameView.js
+++ src/views/GameView.js
@@ -6,21 +6,44 @@ import useStore from '../store/store'
 import styles from './game.module.scss'
 import useGameState from '../hooks/useGameState'
 
+import {
+	SwitchTransition,
+	TransitionGroup,
+	CSSTransition
+} from 'react-transition-group'
 /**
  * Default game view. Props take presedence, this is used to display the game preview in the OptionsView component.
  */
 const GameView = () => {
-	const recipe = useStore((state) => state.recipe)
-	const { playerSecondKeyCorrect } = useGameState()
+	const { recipe, score, scoreLimit } = useStore((state) => ({
+		recipe: state.recipe,
+		score: state.score,
+		scoreLimit: state.scoreLimit
+	}))
+	const {
+		firstKey,
+		secondKey,
+		playerKeyIncorrect,
+		playerSecondKeyCorrect,
+		currentBuilding
+	} = useGameState()
 
 	return recipe ? (
 		<div
 			className={`${styles.container} ${
 				playerSecondKeyCorrect ? 'scored' : null
 			}`}>
-			<Score />
-			<Prompt />
-			<Keys />
+			{/* <Score score={score} scoreLimit={scoreLimit}/> */}
+			<Prompt
+				name={currentBuilding.name}
+				icon={currentBuilding.icon}
+				type={currentBuilding.type}
+			/>
+			<Keys
+				firstKey={firstKey}
+				secondKey={secondKey}
+				playerKeyIncorrect={playerKeyIncorrect}
+			/>
 		</div>
 	) : (
 		<div> nothin here mate</div>
@@ -30,12 +53,7 @@ const GameView = () => {
 /**
  * Displays the current score and scorelimit (-> indicate how many buildings are left)
  */
-const Score = () => {
-	const { score, scoreLimit } = useStore((state) => ({
-		score: state.score,
-		scoreLimit: state.scoreLimit
-	}))
-
+const Score = ({ score, scoreLimit }) => {
 	const gridColumns = scoreLimit === 25 ? 5 : scoreLimit === 50 ? 10 : 20
 
 	return (
@@ -57,31 +75,35 @@ const Score = () => {
 /**
  * Displays the current building prompt
  */
-const Prompt = (props) => {
-	const { currentBuilding } = useGameState()
-	const { type } = currentBuilding || ''
-
-	const name = props.name || currentBuilding.name
-	const icon = props.icon || currentBuilding.icon
+const Prompt = ({ name, icon, type }) => {
+	const score = useStore((state) => state.score)
 
 	return (
-		<div className={styles['building-column']}>
-			<div
-				className={`
+		<TransitionGroup>
+			<CSSTransition key={score} timeout={500} classNames="fade">
+				<div className={styles['building-column']}>
+					<div
+						className={`
 				${styles['icon-container']}
 				${styles[type]}
 				`}>
-				<img className={styles.icon} src={icon} alt={name + ' icon'} />
-			</div>
-			<div className={styles.name}>{name}</div>
-		</div>
+						<img
+							className={styles.icon}
+							src={icon}
+							alt={name + ' icon'}
+						/>
+					</div>
+					<div className={styles.name}>{name}</div>
+				</div>
+			</CSSTransition>
+		</TransitionGroup>
 	)
 }
 
 /**
  * Displays the key indicators
  */
-const Keys = (props) => {
+const Keys = ({ firstKey, secondKey, playerKeyIncorrect }) => {
 	const { keyPressed, score, gameState, showKeyLabels } = useStore(
 		(state) => ({
 			keyPressed: state.keyPressed,
@@ -90,8 +112,8 @@ const Keys = (props) => {
 			showKeyLabels: state.showKeyLabels
 		})
 	)
-	const { firstKey, secondKey, playerKeyIncorrect } = useGameState()
-	const hotkeys = props.hotkeys || [firstKey, secondKey]
+
+	const hotkeys = [firstKey, secondKey]
 
 	const keyLabels =
 		showKeyLabels === 'SHOW'
