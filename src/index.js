d70c1e270c801d17ef5c88b01e4c4b13ac098e12
Alexander Widua <a.widua@gmail.com>
2021-12-29 17:13:57 +0100

    style: reformat animations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/base.scss'
-import './styles/animation.css'
+import './styles/animation.scss'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
