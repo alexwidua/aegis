ea099a4f53f118fb854618bb2f3358409b5b86ae
Alexander Widua <a.widua@gmail.com>
2021-12-29 14:22:29 +0100

    style: change score styling

--- src/views/GameView.js
+++ src/views/GameView.js
@@ -6,19 +6,18 @@ import useStore from '../store/store'
 import styles from './game.module.scss'
 import useGameState from '../hooks/useGameState'
 
-import {
-	SwitchTransition,
-	TransitionGroup,
-	CSSTransition
-} from 'react-transition-group'
+import Button from '../components/ui/Button'
+
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 /**
  * Default game view. Props take presedence, this is used to display the game preview in the OptionsView component.
  */
 const GameView = () => {
-	const { recipe, score, scoreLimit } = useStore((state) => ({
+	const { recipe, score, scoreLimit, handleGameEnd } = useStore((state) => ({
 		recipe: state.recipe,
 		score: state.score,
-		scoreLimit: state.scoreLimit
+		scoreLimit: state.scoreLimit,
+		handleGameEnd: state.handleGameEnd
 	}))
 	const {
 		firstKey,
@@ -33,7 +32,6 @@ const GameView = () => {
 			className={`${styles.container} ${
 				playerSecondKeyCorrect ? 'scored' : null
 			}`}>
-			{/* <Score score={score} scoreLimit={scoreLimit}/> */}
 			<Prompt
 				name={currentBuilding.name}
 				icon={currentBuilding.icon}
@@ -44,6 +42,12 @@ const GameView = () => {
 				secondKey={secondKey}
 				playerKeyIncorrect={playerKeyIncorrect}
 			/>
+			<div className={styles.bottom}>
+				<Score score={score} scoreLimit={scoreLimit} />
+				<Button onClick={handleGameEnd} tertiary>
+					End game
+				</Button>
+			</div>
 		</div>
 	) : (
 		<div> nothin here mate</div>
@@ -54,21 +58,11 @@ const GameView = () => {
  * Displays the current score and scorelimit (-> indicate how many buildings are left)
  */
 const Score = ({ score, scoreLimit }) => {
-	const gridColumns = scoreLimit === 25 ? 5 : scoreLimit === 50 ? 10 : 20
-
 	return (
-		<div
+		<span
 			className={styles.score}
-			style={{ gridTemplateColumns: `repeat(${gridColumns}, 1fr)` }}>
-			{[...Array(scoreLimit)].map((_, i) => (
-				<span
-					className={`${styles['score-item']} ${
-						score >= i + 1 ? styles.cleared : null
-					}`}
-					key={i}
-				/>
-			))}
-		</div>
+			dangerouslySetInnerHTML={{ __html: `${score} / ${scoreLimit}` }}
+		/>
 	)
 }
 
@@ -79,9 +73,9 @@ const Prompt = ({ name, icon, type }) => {
 	const score = useStore((state) => state.score)
 
 	return (
-		<TransitionGroup>
-			<CSSTransition key={score} timeout={500} classNames="fade">
-				<div className={styles['building-column']}>
+		<>
+			<TransitionGroup>
+				<CSSTransition key={score} timeout={500} classNames="fade">
 					<div
 						className={`
 				${styles['icon-container']}
@@ -93,10 +87,10 @@ const Prompt = ({ name, icon, type }) => {
 							alt={name + ' icon'}
 						/>
 					</div>
-					<div className={styles.name}>{name}</div>
-				</div>
-			</CSSTransition>
-		</TransitionGroup>
+				</CSSTransition>
+			</TransitionGroup>
+			<div className={styles.name}>{name}</div>
+		</>
 	)
 }
 
