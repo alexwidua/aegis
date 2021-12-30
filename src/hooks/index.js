46b67b1ad134d3f4d063bf13ca086e9d05c1ab52
Alexander Widua <a.widua@gmail.com>
2021-12-30 15:34:16 +0100

    refactor: module export

---
+++ src/hooks/index.js
@@ -0,0 +1,6 @@
+import useGameState from './useGameState'
+import useLocalStorage from './useLocalStorage'
+import useWindowSize from './useWindowSize'
+import useEffectOnce from './useEffectOnce'
+
+export { useGameState, useLocalStorage, useWindowSize, useEffectOnce }
