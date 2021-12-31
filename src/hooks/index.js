7e94406ef83273cfa8bfda5b8e2a680bfcaec106
Alexander Widua <a.widua@gmail.com>
2021-12-31 17:03:21 +0100

    fix: add missing hook import

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,11 +3,13 @@ import useLocalStorage from './useLocalStorage'
 import useWindowSize from './useWindowSize'
 import useEffectOnce from './useEffectOnce'
 import useOnClickOutside from './useOnClickOutside'
+import useKeyPress from './useKeyPress'
 
 export {
 	useGameState,
 	useLocalStorage,
 	useWindowSize,
 	useEffectOnce,
-	useOnClickOutside
+	useOnClickOutside,
+	useKeyPress
 }
