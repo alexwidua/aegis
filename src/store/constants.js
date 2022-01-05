96bd74f06f9968f649afa1506de69daba5e47109
Alexander Widua <a.widua@gmail.com>
2022-01-05 17:30:05 +0100

    misc: move const values to dedicated file

---
+++ src/store/constants.js
@@ -0,0 +1,6 @@
+// Key used to store settings in local storage
+// Changing the key will 'erase' all local user stored preference
+export const LOCAL_STORAGE_KEY = 'aoe-shortcuts-v060'
+
+// Treshold before displaying game stats after game ended.
+export const MIN_BUILDING_TRESHOLD = 15
